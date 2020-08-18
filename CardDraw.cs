6e6b0a51f1f15f349f81ca399bc8cd166057a5b6
matthew-bvrn <foorganders@outlook.com>
2020-08-18 19:02:40 +0100

    Cards are now positioned correctly on screen

--- CardDraw.cs
+++ CardDraw.cs
@@ -17,14 +17,17 @@ public class CardDraw : Node2D
 		List<Card> playerHand = ((Player)GetParent()).hand;
 		atlasTexture.Atlas = texture;
 
-
+		Vector2 screenSize=GetViewport().Size;
+        GD.Print(screenSize);
+        GD.Print(((Node2D)GetParent()).Position);
+        GD.Print((((Node2D)GetParent()).Position)-screenSize/2);
 		int count = 0;
 		foreach (Card i in playerHand)
 		{
 
 				atlasTexture.Region = new Rect2((i.rank-2) * 16, i.suit * 16, 16,16);
 
-				DrawTexture(atlasTexture, new Vector2((count%8+1) * 16, (count/8)*16));
+				DrawTexture(atlasTexture, new Vector2((-screenSize.x/4)+(screenSize.x/2-16*8)+((count%8+1) * 16)-26, +10+(-screenSize.y/4)+((count/8)*16)));
 				count++;
 
 		}
