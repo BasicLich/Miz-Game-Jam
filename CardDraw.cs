158439a7c0ff70341d250b7a523113d31a7bdc76
matthew-bvrn <foorganders@outlook.com>
2020-08-18 19:29:43 +0100

    cards now have a gui box

--- CardDraw.cs
+++ CardDraw.cs
@@ -9,7 +9,8 @@ public class CardDraw : Node2D
 
 	// Called when the node enters the scene tree for the first time.
 	Texture texture = GD.Load<Texture>("res://Tiles/cards.png");
-	AtlasTexture atlasTexture = new AtlasTexture();
+    Texture back= GD.Load<Texture>("res://Tiles/cardbackdrop.png");
+    AtlasTexture atlasTexture = new AtlasTexture();
 
 	
 	public override void _Draw()
@@ -22,12 +23,19 @@ public class CardDraw : Node2D
         GD.Print(((Node2D)GetParent()).Position);
         GD.Print((((Node2D)GetParent()).Position)-screenSize/2);
 		int count = 0;
-		foreach (Card i in playerHand)
+
+        Vector2 baseLoc = new Vector2((-screenSize.x / 4) + (screenSize.x / 2 - 16 * 8) - 32-10, 10 + (-screenSize.y / 4));
+
+        DrawTexture(back, baseLoc);
+
+        foreach (Card i in playerHand)
 		{
 
 				atlasTexture.Region = new Rect2((i.rank-2) * 16, i.suit * 16, 16,16);
+                Vector2 drawLoc = baseLoc+ new Vector2( ((count % 8 + 1) * 16), ((count / 8+1) * 16));
+                
 
-				DrawTexture(atlasTexture, new Vector2((-screenSize.x/4)+(screenSize.x/2-16*8)+((count%8+1) * 16)-26, +10+(-screenSize.y/4)+((count/8)*16)));
+                DrawTexture(atlasTexture, drawLoc);
 				count++;
 
 		}
