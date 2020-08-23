92ae50cbe6a5b9d1b4f5b206d63f92bc801fcbb1
matthew-bvrn <foorganders@outlook.com>
2020-08-23 14:16:24 +0100

    rename selectable menu item

--- SelectableMenuItem.cs
+++ SelectableMenuItem.cs
@@ -1,7 +1,7 @@
 using Godot;
 using System;
 
-public class ClickableMenu: TextureRect
+public class SelectableMenuItem : TextureRect
 {
 	// Declare member variables here. Examples:
 	// private int a = 2;
