fa37b371ea0bdbee53c37b3ce5cbacd3b66226df
matthew-bvrn <foorganders@outlook.com>
2020-08-26 21:28:08 +0100

    Bugfix - Palette unlock states no longer triggered in lose state

--- WinLose.cs
+++ WinLose.cs
@@ -9,65 +9,64 @@ public class WinLose : Label
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
-	{
-		var saveGame = new File();
-		
-		saveGame.Open("user://savegame.save", File.ModeFlags.ReadWrite);
+	{	
 		string unlockString = "";
 		Global.floorLevel = 0;
-		//Check for unlocks
-		if (Global.win && !Global.paletteUnlocked[0])
-		{
-			saveGame.Store8(1);
-			unlockString += "Autumn Memories Palette Unlocked! Palettes Can Be Swapped In Options Menu\n\n";
-			Global.paletteUnlocked[0] = true;
-		}
-		else
-		{
-			saveGame.Seek(1);
+        //Check for unlocks
+        if (global.win)
+        {
+            var saveGame = new File();
+            saveGame.Open("user://savegame.save", File.ModeFlags.ReadWrite);
 
-		}
-		if (Global.score>=1500 && !Global.paletteUnlocked[1])
-		{
-			unlockString += "Muted Thoughts Palette Unlocked!\n\n";
-			saveGame.Store8(1);
-			Global.paletteUnlocked[1] = true;
-		}
-		else
-		{
-			saveGame.Seek(2);
-		}
-		if (Global.score >= 2000 && !Global.paletteUnlocked[2])
-		{
-			unlockString += "Blooded Nose Palette Unlocked!\n\n";
-			saveGame.Store8(1);
-			Global.paletteUnlocked[2] = true;
-		}
-		else
-		{
-			saveGame.Seek(3);
-		}
-		if (Global.score >= 2500 && !Global.paletteUnlocked[3])
-		{
-			unlockString += "90s Nostalgia Palette Unlocked!\n\n";
-			saveGame.Store8(1);
-			Global.paletteUnlocked[3] = true;
-		}
-		else
-		{
-			saveGame.Seek(4);
-		}
+            if (!Global.paletteUnlocked[0])
+            {
+                saveGame.Store8(1);
+                unlockString += "Autumn Memories Palette Unlocked! Palettes Can Be Swapped In Options Menu\n\n";
+                Global.paletteUnlocked[0] = true;
+            }
+            else
+            {
+                saveGame.Seek(1);
 
-		if (Global.score>Global.highScore && Global.win)
-		{
-			unlockString += "New Highscore!";
-			saveGame.Store32((uint)Global.score);
-			Global.highScore = Global.score;
-		}
-		else
-		{
-			saveGame.GetLine();
-		}
+            }
+            if (Global.score >= 1500 && !Global.paletteUnlocked[1])
+            {
+                unlockString += "Muted Thoughts Palette Unlocked!\n\n";
+                saveGame.Store8(1);
+                Global.paletteUnlocked[1] = true;
+            }
+            else
+            {
+                saveGame.Seek(2);
+            }
+            if (Global.score >= 2000 && !Global.paletteUnlocked[2])
+            {
+                unlockString += "Blooded Nose Palette Unlocked!\n\n";
+                saveGame.Store8(1);
+                Global.paletteUnlocked[2] = true;
+            }
+            else
+            {
+                saveGame.Seek(3);
+            }
+            if (Global.score >= 2500 && !Global.paletteUnlocked[3])
+            {
+                unlockString += "90s Nostalgia Palette Unlocked!\n\n";
+                saveGame.Store8(1);
+                Global.paletteUnlocked[3] = true;
+            }
+            else
+            {
+                saveGame.Seek(4);
+            }
+
+            if (Global.score > Global.highScore)
+            {
+                unlockString += "New Highscore!";
+                saveGame.Store32((uint)Global.score);
+                Global.highScore = Global.score;
+            }
+        }
 		((Label)GetParent().GetNode("UnlockText")).Text = unlockString;
 
 		((TextureRect)GetParent().FindNode("Continue")).Modulate = new Color(1.3f, 1.3f, 1.3f, 1.3f);
